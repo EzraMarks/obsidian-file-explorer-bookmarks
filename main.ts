c7b42c6989f2425cbd37957e151e86ad5e7ed5f4
Ezra Marks <ezrablankmarks@gmail.com>
2025-06-07 18:29:18 -0400

    Initial draft of stacked sidebar plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,124 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	Plugin,
+	WorkspaceLeaf,
+	ItemView,
+} from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+const STACKED_SIDEBAR_VIEW_TYPE = 'stacked-sidebar-view';
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class StackedSidebarPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
+		this.registerView(
+			STACKED_SIDEBAR_VIEW_TYPE,
+			(leaf) => new StackedSidebarView(leaf)
+		);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			id: 'open-stacked-sidebar',
+			name: 'Open Stacked Sidebar',
+			callback: async () => {
+				await this.app.workspace.ensureSideLeaf(STACKED_SIDEBAR_VIEW_TYPE, "left", { active: true });
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+	}
+}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+class StackedSidebarView extends ItemView {
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
+
+	override getViewType() {
+		return STACKED_SIDEBAR_VIEW_TYPE;
+	}
+
+	override getDisplayText() {
+		return 'Files (Stacked Sidebar)';
+	}
+
+	override getIcon() {
+		return 'folder-tree';
+	}
+
+	// Now an array of objects with viewType and optional label
+	private embeddedViews = [
+		{ viewType: "file-explorer", label: undefined },
+		{ viewType: "bookmarks", label: "Bookmarks" }
+	];
+
+	override async onOpen() {
+		this.initialize();
+	}
+
+	async initialize() {
+		const container = this.containerEl;
+		container.empty();
+		const wrapper = container.createDiv('stacked-sidebar-wrapper');
+		const combinedBody = wrapper.createDiv('stacked-sidebar-body');
+		const combinedFooter = wrapper.createDiv('stacked-sidebar-footer');
+
+		let mainButtonsContainer: HTMLElement | null = null;
+
+		for (const { viewType, label } of this.embeddedViews) {
+			const shadowLeaf = await this.makeShadowLeaf(viewType);
+			const shadowContainerEl = shadowLeaf.view.containerEl;
+			shadowContainerEl.className = ""; // remove all formatting
+
+			const header = shadowContainerEl.querySelector('.nav-header') as HTMLElement | null;
+			const buttonsContainer = header?.querySelector('.nav-buttons-container') as HTMLElement | null;
+
+			// Combine nav-buttons-containers into the header
+			if (buttonsContainer) {
+				if (!mainButtonsContainer) {
+					mainButtonsContainer = buttonsContainer;
+					combinedFooter.replaceWith(header!);
+					combinedFooter.addClass("stacked-sidebar-footer");
+				} else {
+					// For subsequent views, add their buttons to the main container
+					// while (buttonsContainer.firstChild) {
+					// 	mainButtonsContainer.appendChild(buttonsContainer.firstChild);
+					// }
+					header?.remove();
 				}
 			}
+
+			// Add section label if defined
+			if (label) {
+				const sectionLabel = document.createElement('div');
+				sectionLabel.className = 'stacked-sidebar-section-label';
+				sectionLabel.textContent = label;
+				combinedBody.appendChild(sectionLabel);
+			}
+
+			// Add the full view content to the combined body
+			combinedBody.appendChild(shadowContainerEl);
+		}
+	}
+
+	override async onClose() {
+		this.embeddedViews.forEach(({ viewType }) => {
+			const shadowViewType = getShadowViewType(viewType)
+			this.app.workspace.getLeavesOfType(shadowViewType).forEach(leaf => {
+				leaf.detach()
+			});
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
-	onunload() {
+	async makeShadowLeaf(viewType: string): Promise<WorkspaceLeaf> {
+		const leaf = this.app.workspace.getLeftLeaf(false)!;
+		leaf.getDisplayText = () => "stacked-sidebar-helper";
+		leaf.getIcon = () => "";
 
-	}
+		await leaf.setViewState({ type: viewType });
+		const shadowViewType = getShadowViewType(viewType);
+		leaf.view.getViewType = () => shadowViewType;
+		// If this shadow is closed, close the whole thing.
+		leaf.view.onunload = () => this.leaf.detach();
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+		return leaf;
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+const getShadowViewType = (viewType: string) => {
+	return `shadow_${viewType}`;
 }
